7bc0307ea430dc45e7cd3a3fc96441aaced140e1
Ahnaf Al Nafis <ahnafalnafis@gmail.com>
2023-04-06 15:53:25 +0600

    Added PostCSS

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: [
+    require("autoprefixer"),
+    require("cssnano"),
+    require("postcss-import"),
+    require("postcss-preset-env")({
+      stage: 1,
+    }),
+  ],
+};

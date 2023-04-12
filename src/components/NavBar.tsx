7f986a3daaa05c5acc644ee0a3e89cb19d2a13f1
Ahnaf Al Nafis <ahnafalnafis@gmail.com>
2023-04-12 21:57:01 +0600

    Chore :)

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class NavBar extends Component {
+  render(): React.ReactNode {
+    return (
+      <React.Fragment>
+        <nav>
+          <ul></ul>
+        </nav>
+      </React.Fragment>
+    );
+  }
+}
+
+export default NavBar;
